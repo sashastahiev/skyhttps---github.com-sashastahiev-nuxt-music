79365716232e56c93198fe3082264bf1fa17d3e0
sashastahiev <vebium@mail.ru>
2025-10-05 14:56:14 +0300

    commit styles 05.10.25

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  css: ["./public/styles/main.css"],
   devtools: { enabled: true }
 })
